6f01521905d3f8f2e5515a4b05541cca627eb668
Mason Wheaton <mlwheaton06@gmail.com>
2023-01-24 21:48:02 -0600

    Add project files.

---
+++ Calculator/Calculate.cs
@@ -0,0 +1,36 @@
+﻿namespace Calculator;
+
+public class Calculate
+{
+    public void Multiply()
+    {
+        Console.Write("Please enter the numbers to multiply, separated by commas: ");
+        string input = Console.ReadLine();
+        string[] splitList = input.Split(",");
+
+        int total = 1;
+        for (int i = 0; i < splitList.Length; i++)
+        {
+            int num = int.Parse(splitList[i]);
+            total = total * num;
+        }
+
+        Console.WriteLine(total);
+    }
+
+    public void Square()
+    {
+        Console.Write("Please enter the numbers to square, separated by commas: ");
+        string input = Console.ReadLine();
+        string[] splitList = input.Split(",");
+
+        List<int> squareList= new List<int>();
+        for (int i = 0; i < splitList.Length; i++)
+        {
+            int num = int.Parse(splitList[i]);
+            squareList.Add(num * num);
+        }
+        string squareListJoined = String.Join(",", squareList);
+        Console.WriteLine(squareListJoined);
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculator;
+
+Calculate calculator = new Calculate();
+
+Console.WriteLine(@"Would you like to
+1. Multiply
+2. Square
+?");
+string inputCalculate = Console.ReadLine();
+
+if (inputCalculate == "1")
+{
+    calculator.Multiply();
+}
+else if (inputCalculate == "2")
+{
+    calculator.Square();
+}
